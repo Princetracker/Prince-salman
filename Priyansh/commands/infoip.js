7746020daf0fa85e1fabc37ae803ff7ed818a30a
Princetracker <salmankhan.03172012@gmail.com>
2025-02-22 14:50:28 +0500

    Update infoip.js

--- Priyansh/commands/infoip.js
+++ Priyansh/commands/infoip.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "infoip",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "— ※ 𝐁͢ɼ๏͜͡ʞɛ̈n͎ 𝐏͢ɼıı͜͡ƞɕɘ ː͢» 🩷🪽",
 	description: "get info of ip address",
   usages: "[ip address]",
 	commandCategory: "...",
